f4d8d1b1dac6e4a5f76edffea59b7dde6bd35f1c
Poojavishv <118157946+Poojavishv@users.noreply.github.com>
2023-04-04 10:40:57 +0530

    Master Commit

---
+++ src/main/java/com/example/demo/TemplateDemo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateDemo2Application.class, args);
+	}
+
+}
