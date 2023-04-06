a1df54006e2f93e0409489f1f4fb459aa4f86afe
Poojavishv <118157946+Poojavishv@users.noreply.github.com>
2023-04-06 11:10:44 +0530

    Controller is edited

--- src/main/java/com/example/demo/controllers/MyController.java
+++ src/main/java/com/example/demo/controllers/MyController.java
@@ -5,28 +5,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-<<<<<<< HEAD
+
 import com.example.demo.model.Doctor;
 import com.example.demo.model.Patient;
 import com.example.demo.service.DoctorService;
 import com.example.demo.service.PatientService;
-=======
+
 import com.example.demo.service.AdminService;
->>>>>>> branch 'master' of https://github.com/Poojavishv/PatientHealthMonitoringPortal.git
 
 @Controller
 public class MyController {
-<<<<<<< HEAD
 
 	@Autowired
 	DoctorService docService;
 	@Autowired
 	PatientService patientService;
 
-=======
+
 	@Autowired
 	AdminService adService;
->>>>>>> branch 'master' of https://github.com/Poojavishv/PatientHealthMonitoringPortal.git
+
+
 	@RequestMapping("/")
 	public ModelAndView indexView() {
 		ModelAndView mv = new ModelAndView("index");
@@ -51,21 +50,6 @@ public class MyController {
 		return mv;
 	}
 
-	@RequestMapping("/hospitaladminlogin")
-	public ModelAndView hospitalloginView() {
-		ModelAndView mv = new ModelAndView("hospitaladminlogin");
-		return mv;
-	}
-
-	@RequestMapping("/doctorlogin")
-	public ModelAndView doctorloginView() {
-		ModelAndView mv = new ModelAndView("doctorlogin");
-		mv.addObject("smsg", "");
-		mv.addObject("emsg", "");
-		return mv;
-
-	}
-
 	@RequestMapping("/loginCheckDoctor")
 	public ModelAndView doctorLoginCheck(String docEmail, String docPassword) {
 		ModelAndView mv = new ModelAndView();
@@ -78,7 +62,7 @@ public class MyController {
 			mv.addObject("emsg", "Invalid Username or Password");
 			return mv;
 		}
-<<<<<<< HEAD
+
 	}
 
 	@RequestMapping("/loginCheckPatient")
@@ -91,24 +75,19 @@ public class MyController {
 			mv.setViewName("userlogin");
 			mv.addObject("smsg", "");
 			mv.addObject("emsg", "Invalid Username or Password");
-=======
-		@RequestMapping("/hospitaladminlogin")
-		public ModelAndView hospitalloginView() {
-			ModelAndView mv=new ModelAndView("hospitaladminlogin");
-			mv.addObject("smsg","");
-			mv.addObject("emsg","");
-			mv.addObject("name", mv);		
->>>>>>> branch 'master' of https://github.com/Poojavishv/PatientHealthMonitoringPortal.git
 			return mv;
 		}
-<<<<<<< HEAD
 	}
-
-	@RequestMapping("/doctorregistration")
-	public ModelAndView doctorregView() {
-		ModelAndView mv = new ModelAndView("doctorregistration");
+		
+	@RequestMapping("/hospitaladminlogin")
+	public ModelAndView hospitalloginView() {
+		ModelAndView mv = new ModelAndView("hospitaladminlogin");
+		mv.addObject("smsg", "");
+		mv.addObject("emsg", "");
+		mv.addObject("name", mv);
 		return mv;
 	}
+
 
 	@RequestMapping("/doctorReg")
 	public ModelAndView doctorRegistration(Doctor doc) {
@@ -140,56 +119,52 @@ public class MyController {
 		return mv;
 	}
 
+
+	@RequestMapping("/loginCheck")
+	public ModelAndView loginCheckView(String userid, String password) {
+		ModelAndView mv = new ModelAndView();
+		if (adService.adminLoginCheck(userid, password)) {
+
+			mv.setViewName("adminHome");
+
+			return mv;
+		}
+
+		else {
+			mv.setViewName("hospitaladminlogin");
+			mv.addObject("smsg", "");
+			mv.addObject("emsg", "Invalid userid or password");
+			return mv;
+		}
+
+	}
+
+	@RequestMapping("/doctorlogin")
+	public ModelAndView doctorloginView1() {
+		ModelAndView mv = new ModelAndView("doctorlogin");
+		return mv;
+	}
+
+	@RequestMapping("/doctorlogin")
+	public ModelAndView doctorloginView() {
+		ModelAndView mv = new ModelAndView("doctorlogin");
+		mv.addObject("smsg", "");
+		mv.addObject("emsg", "");
+		return mv;
+
+	}
+
+	@RequestMapping("/doctorregistration")
+	public ModelAndView doctorregView() {
+		ModelAndView mv = new ModelAndView("doctorregistration");
+		return mv;
+	}
+
+
 	@RequestMapping("/forgotPassword1")
 	public ModelAndView forgotPassword1View() {
 		ModelAndView mv = new ModelAndView("forgotPassword1");
 		return mv;
 	}
-=======
-	@RequestMapping("/loginCheck")
-	public ModelAndView loginCheckView(String userid,String password) {
-		ModelAndView mv=new ModelAndView();
-		if(adService.adminLoginCheck(userid, password)) {
-			
-			mv.setViewName("adminHome");
-			
-			return mv;
-		}
-		
-		else {
-			mv.setViewName("hospitaladminlogin");
-			mv.addObject("smsg","");
-			mv.addObject("emsg","Invalid userid or password");
-			return mv;
-		}
-		
-	}
-		@RequestMapping("/doctorlogin")
-		public ModelAndView doctorloginView() {
-			ModelAndView mv=new ModelAndView("doctorlogin");
-			return mv;
-		}
-		@RequestMapping("/doctorregistration")
-		public ModelAndView doctorregView() {
-			ModelAndView mv=new ModelAndView("doctorregistration");
-			return mv;
-		}
-		@RequestMapping("/userreg")
-		public ModelAndView userRegView() {
-			ModelAndView mv=new ModelAndView("userreg");
-			return mv;
-		}
-		
-		@RequestMapping("/hospitalAdminRegister")
-		public ModelAndView hospitalAdminRegisterView() {
-			ModelAndView mv=new ModelAndView("hospitalAdminRegister");
-			return mv;
-		}
-		
-		@RequestMapping("/forgotPassword1")
-		public ModelAndView forgotPassword1View() {
-			ModelAndView mv=new ModelAndView("forgotPassword1");
-			return mv;
-		}
->>>>>>> branch 'master' of https://github.com/Poojavishv/PatientHealthMonitoringPortal.git
+
 }
